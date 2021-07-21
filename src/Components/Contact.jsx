2ec1be549ddb439a4fa7da02c6e011cd1875b303
Maya Hagan <miyaclessa@gmail.com>
2021-07-22 06:33:41 +1200

    update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,13 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { Button } from 'react-bootstrap';
+import Card from "react-bootstrap/Card";
+import Modal from 'react-bootstrap/esm/Modal';
+import ListGroup from "react-bootstrap/ListGroup";
+import EditForm from '../Components/EditForm'
 
-const Contact = ({contact,index}) => {
+const Contact=({ contact, handleDeleteContact, handleEditContact}) => {
+
+  
+    const [show, setshow]= useState(false);
+    const handleClose = () => setshow(false);
+    const handleShow = () => setshow(true);
+    const deleteHandle = (e) => {
+        handleDeleteContact(contact.id)
+    }
     return (
-        <div className='contact' key={index}>
-        <h3>Name: {contact.name}</h3>
-        <h3>Number: {contact.number}</h3>
-        <h3>Location: {contact.location}</h3><hr/>
-    </div>
-    );
+        <>
+        <Card className="contact col-md-3"> 
+          <Card.Header>Name: {contact.Name}</Card.Header>
+          <ListGroup variant="flush">
+          <ListGroup.Item>Phone: {contact.Phone}</ListGroup.Item>
+          <ListGroup.Item>Location: {contact.Location}</ListGroup.Item>
+          </ListGroup>
+          <Button onClick={deleteHandle}>Delete</Button><br />
+          <Button onClick={handleShow}>Edit</Button>
+          </Card>
+
+          <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                  <Modal.Title>Edit Contact</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+              <EditForm 
+              contact={contact}
+              handleEditContact={handleEditContact}
+              closeModal={handleClose}
+              />
+              </Modal.Body>
+          </Modal>
+        </>
+    )
 }
 
 export default Contact;
