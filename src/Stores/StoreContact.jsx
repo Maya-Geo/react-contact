286b7913e3d541593725dee34333f141f364fcdf
Maya Hagan <miyaclessa@gmail.com>
2021-08-21 02:34:08 +1200

    delete

--- src/Stores/StoreContact.jsx
+++
@@ -1,9 +0,0 @@
-import ContactReducer from '../Reducer/ContactReducer';
-import {createStore} from "redux"
-
-
-const StoreContact = createStore(ContactReducer) 
-   
-       
-
-export default StoreContact;
