436bf01bca3bb39aaa9501c70e310f63eb64e856
Maya Hagan <miyaclessa@gmail.com>
2021-08-21 02:32:37 +1200

    update

--- src/Components/EditForm.jsx
+++ src/Components/EditForm.jsx
@@ -13,50 +13,43 @@ export default class ContactForm extends Component {
                     id: props.contact.id || '',
                 }
     };
-    handleChange=(e)=>{
-     this.setState({[e.target.name] : e.target.value})
+
+    handleChange= (e) => {
+     this.setState({ [name] :value })
+     const target = e.target;
+     const value = target.value;
+     const name = target.name;
     };
+
+
     handleSubmit=(e)=>{
         e.preventDefault();
-        this.props.handleEditContact(this.state);
+        this.props.handeEditContact(this.state);
         this.setState({
+
         });
         this.props.closeModal();
+        alert("Contact edited")
     }
     render() {
         return (
             <>
-     <Form onSubmit={this.handleSubmit}>
-         <Form.Group controlId="formBasicName">
-         <Form.Label htmlFor="name">Name</Form.Label>
-         <Form.Control type="text" name="Name" htmlFor="Name"
-         placeholder="Name" className="input"
-         onChange={this.handleChange} 
-         value={this.state.Name} 
-         />
-         </Form.Group>
-         <Form.Group controlId="formBasicNumber">
-          <Form.Label htmlFor="Number">Phone</Form.Label>
-         <Form.Control type="number" name="Phone" htmlFor="Phone"
-         className="input"
-         placeholder="Phone"
-         onChange={this.handleChange} 
-         value={this.state.Phone} 
-         />
-         </Form.Group>
-         <Form.Group controlId="formBasicLocation">
-             <Form.Label htmlFor="Location"></Form.Label>
-         <Form.Control type="text" name="Location" htmlFor="Location" 
-         className="input"
-         placeholder="Location" 
-         onChange={this.handleChange} 
-         value={this.state.Location} 
-         />
-         </Form.Group>
-         <div className="form-control">
-            <Button variant="primary" type="submit">Add To Contact</Button>
-         </div>
-     </Form>
+               <div className="form">
+                <form onSubmit={this.handleSubmit}>
+                    <label htmlFor="name">Name</label><br/>
+                    <input type="text" name="name"  value={this.state.name} 
+                    placeholder="Your Name" onChange={this.handleChange}/><br/>
+                    
+                    <label htmlFor="number">Number</label><br/>
+                    <input type="text" name="number" value={this.state.number} 
+                    onChange={this.handleChange} placeholder="Number"/><br/>
+
+                    <label htmlFor="location">Location</label><br/>
+                    <input type="text" name="location" value={this.state.location} 
+                    onChange={this.handleChange} placeholder="Your Location"/>
+                     <Button variant="primary" type="submit">Add To Contact</Button>
+                </form>
+            </div>
             </>
         )
     }
