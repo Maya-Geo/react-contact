6b9b21e46c97ceea510212eb625971958b5c8473
Maya Hagan <miyaclessa@gmail.com>
2021-08-28 08:15:40 +1200

    add delete amd edit

--- src/Reducer/ContactReducer.jsx
+++ src/Reducer/ContactReducer.jsx
@@ -8,9 +8,9 @@ const initialState = {
         },
 
         {
-            name: "Maya",
-            number: "050000000",
-            location: "Anagkazo",
+            name: "Liti",
+            number: "02000000",
+            location: "Lees",
             id: Math.random().toString()
           }
 
@@ -22,11 +22,31 @@ const initialState = {
 const contactReducer = (state = initialState, action) => {
     switch (action.type) {
         case "ADD_CONTACT":
-            return state
+            return  { ...state.contacts, contacts: [...state.contacts, action.payload] };
+
+
+            case "EDIT_CONTACT":
+             const newContacts = state.contacts.map((contact) =>
+            contact.id === action.payload.id ? action.payload : contact
+          )
+
+
+          return {...state, contacts: newContacts};
+    
+          case "DELETE_CONTACT":
+            const contactSaved = state.contacts.filter((contact) => {
+              return contact.id !== action.payload;
+            }); 
+            return {...state, contacts: contactSaved};
+
+
+
+
+
     
         default:
             return state;
     }
-}
+} 
 
 export default contactReducer
