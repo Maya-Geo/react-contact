6fec5fcb713ded4f26184976d751a1de0049f161
Maya Hagan <miyaclessa@gmail.com>
2021-08-28 08:13:40 +1200

    add Edit and Delete

--- src/Actions/ActionContact.jsx
+++ src/Actions/ActionContact.jsx
@@ -6,3 +6,18 @@ export const addContact=(contact)=>{
     }
 }
 
+
+export const editContact = (updatedContact) => {
+    return {
+        type: "EDIT_CONTACT",
+        payload: updatedContact
+    }
+}
+
+
+export const deleteContact = (contact_id) => {
+    return {
+        type: "DELETE_CONTACT",
+        payload: contact_id
+    }
+}
