cf23f6f9617b5275befc0fff5fe1d7dd5167d56c
Maya Hagan <miyaclessa@gmail.com>
2021-09-17 12:04:32 +1200

    update actioncontact

--- src/Actions/ActionContact.jsx
+++ src/Actions/ActionContact.jsx
@@ -1,8 +1,25 @@
+export const ADD_CONTACT = "ADD_CONTACT";
+export const EDIT_CONTACT = "EDIT_CONTACT"
+export const DELETE_CONTACT ="DELETE_CONTACT"
+
+
 export const addContact=(contact)=>{
     contact.id = Math.random().toString()
-    return{
-        type: "ADD_CONTACT",
-        payload: contact
+
+    return (dispatch,state,{getFirestore})=>{
+         const db = getFirestore();
+         db.collection("contacts").add(contact).then(
+            (docs)=>{
+                console.log(docs)
+            })
+
+         .catch((err) => console.log(err));
+
+         
+        // type: "ADD_CONTACT",
+        // payload: contact
+    
+
     }
 }
 
